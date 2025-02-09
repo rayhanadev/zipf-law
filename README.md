ea9d3fa6f6733b7bed79ab84f25448b2714aed1a
ray <me@rayhanadev.com>
2025-02-08 23:57:02 -0500

    feat: add code

---
+++ README.md
@@ -0,0 +1,14 @@
+# Zipf's Law
+
+I saw [this](https://youtu.be/fCn8zs912OE) Vsauce video on Zipf's Law
+and about five minutes in I was like "I need to test that" so naturally
+I loaded up Zed and pulled the text corpus of Macbeth from shakespeare.mit.edu
+to see if it held up.
+
+## Results
+
+I ran the script on Macbeth and got the following results:
+
+![](./word_frequency_distribution.png)
+
+Curiously, it works out pretty well haha! xD
--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from zipf-law!")
+# %%
 
+import string
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
 
-if __name__ == "__main__":
-    main()
+# %%
+
+with open("corpus.txt", "r") as file:
+    content = file.read()
+
+# %%
+
+words = content.translate(str.maketrans("", "", string.punctuation)).split()
+word_counts = Counter(words)
+total_words = sum(word_counts.values())
+relative_frequencies = {
+    word: count / total_words for word, count in word_counts.items()
+}
+
+# %%
+
+sorted_counts = sorted(relative_frequencies.values(), reverse=True)
+ranks = np.arange(1, len(sorted_counts) + 1)
+
+plt.figure(figsize=(6, 6))
+plt.scatter(ranks, sorted_counts, alpha=0.7, color="blue", s=10)
+plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("Word Rank")
+plt.ylabel("Relative Frequency")
+plt.title("Word Frequency Distribution (Zipf's Law)")
+plt.xticks([1, 10, 100, 1000, 10000], labels=["1", "10", "100", "1000", "10000"])
+plt.yticks(
+    [0.00001, 0.0001, 0.001, 0.01, 0.1],
+    labels=["0.00001", "0.0001", "0.001", "0.01", "0.1"],
+)
+plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+plt.show()
